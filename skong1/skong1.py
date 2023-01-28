00aa4a92bd8a58725a0b11c0a544611ef84ac934
vaughn <jwst0208@gmail.com>
2023-01-29 01:00:26 +0900

    add: 예시 코드

--- skong1/skong1.py
+++ skong1/skong1.py
@@ -1,8 +1,9 @@
-"""Welcome to Pynecone! This file outlines the steps to create a basic app."""
 from pcconfig import config
-
 import pynecone as pc
 
+'''
+# Welcome to Pynecone! This file outlines the steps to create a basic app.
+# 기본 예시 페이지 (주석처리)
 docs_url = "https://pynecone.io/docs/getting-started/introduction"
 filename = f"{config.app_name}/{config.app_name}.py"
 
@@ -36,6 +37,50 @@ def index():
 
 
 # Add state and page to the app.
+app = pc.App(state=State)
+app.add_page(index)
+app.compile()
+'''
+
+
+class State(pc.State):
+    total_clicks = 0
+
+    def increase_clicks(self):
+        self.total_clicks += 1
+
+    def decrese_clicks(self):
+        self.total_clicks -= 1
+
+
+def index():
+    return pc.center(
+        pc.vstack(
+            pc.heading(
+                "Click!",
+                font_size="2em",
+                margin_bottom=10,
+            ),
+            pc.hstack(
+                pc.button(
+                    "-",
+                    on_click=State.decrese_clicks,
+                    size="lg",
+                ),
+                pc.text(State.total_clicks),
+                pc.button(
+                    "+",
+                    on_click=State.increase_clicks,
+                    size="lg",
+                ),
+                spacing="20px",
+            ),
+        ),
+        padding_top="10%",
+        font_size="2em",
+    )
+
+
 app = pc.App(state=State)
 app.add_page(index)
 app.compile()
